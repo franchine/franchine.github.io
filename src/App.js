0405e4814caf1bb5e2a570c3591ccb99e73e5b2a
ninhf <ninhf@wwu.edu>
2018-12-18 01:44:24 -0800

    formatting navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './css/App.css';
-import Nav from './Nav';
+import MainNav from './MainNav';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Nav/>
+        <MainNav/>
         {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
