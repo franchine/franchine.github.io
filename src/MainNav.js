14f86e1678493b0386985e7fcb10a71fe9145e7c
ninhf <ninhf@wwu.edu>
2018-12-18 16:29:00 -0800

    favicon changed

--- src/MainNav.js
+++ src/MainNav.js
@@ -1,50 +1,5 @@
-// import React from 'react';
-
-// class Navigation extends React.Component {
-//   render() {
-//     return (
-//       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-//         <a className="navbar-brand" href="/">Franchine Ninh</a>
-//         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-//           <span className="navbar-toggler-icon"></span>
-//         </button>
-
-//         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-//           <ul className="navbar-nav mr-auto">
-//             <li className="nav-item active">
-//               <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
-//             </li>
-//             <li className="nav-item">
-//               <a className="nav-link" href="/">Link</a>
-//             </li>
-//             <li className="nav-item dropdown">
-//               <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-//                 Dropdown
-//               </a>
-//               <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-//                 <a className="dropdown-item" href="/">Action</a>
-//                 <a className="dropdown-item" href="/">Another action</a>
-//                 <div className="dropdown-divider"></div>
-//                 <a className="dropdown-item" href="/">Something else here</a>
-//               </div>
-//             </li>
-//             <li className="nav-item">
-//               <a className="nav-link disabled" href="/">Disabled</a>
-//             </li>
-//           </ul>
-//           <form className="form-inline my-2 my-lg-0">
-//             <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-//             <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-//           </form>
-//         </div>
-//       </nav>
-//     )
-//   }
-// }
-
-// export default Navigation;
 import React from "react";
-import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from "react-bootstrap";
+import { Navbar, Nav, NavItem } from "react-bootstrap";
 import "./css/MainNav.css";
 
 class MainNav extends React.Component {
@@ -53,7 +8,7 @@ class MainNav extends React.Component {
       <Navbar fixedTop>
         <Navbar.Header>
           <Navbar.Brand>
-            <a href="#home">Franchine Ninh</a>
+            <a href="#home">franchine ninh</a>
           </Navbar.Brand>
         </Navbar.Header>
         <Nav className="navItemContainer">
@@ -66,18 +21,9 @@ class MainNav extends React.Component {
           <NavItem eventKey={3} href="#cs">
             visualizations
           </NavItem>
-          <NavItem eventKey={3} href="#contact">
+          <NavItem eventKey={4} href="#contact">
             contact
           </NavItem>
-          {/* <NavDropdown eventKey={3} title="menu" id="basic-nav-dropdown">
-            <div id="dropdown-menu">
-              <MenuItem eventKey={3.1}>Action</MenuItem>
-              <MenuItem eventKey={3.2}>Another action</MenuItem>
-              <MenuItem eventKey={3.3}>Something else here</MenuItem>
-              <MenuItem divider />
-              <MenuItem eventKey={3.4}>Separated link</MenuItem>
-            </div>
-          </NavDropdown> */}
         </Nav>
       </Navbar>
     );
@@ -85,34 +31,3 @@ class MainNav extends React.Component {
 }
 
 export default MainNav;
-
-// class Nav extends React.Component {
-//   render() {
-//     return (
-//       <Navbar>
-//         <Navbar.Header>
-//           <Navbar.Brand>
-//             <a href="#home">Franchine Ninh</a>
-//           </Navbar.Brand>
-//         </Navbar.Header>
-//         <Nav>
-//           <NavItem eventKey={1} href="#">
-//             Link
-//           </NavItem>
-//           <NavItem eventKey={2} href="#">
-//             Link
-//           </NavItem>
-//           <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-//             <MenuItem eventKey={3.1}>Action</MenuItem>
-//             <MenuItem eventKey={3.2}>Another action</MenuItem>
-//             <MenuItem eventKey={3.3}>Something else here</MenuItem>
-//             <MenuItem divider />
-//             <MenuItem eventKey={3.4}>Separated link</MenuItem>
-//           </NavDropdown>
-//         </Nav>
-//       </Navbar>
-//     );
-//   }
-// }
-
-// export default Nav;
