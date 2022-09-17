63613e62a0cca03a44918e5730a03a95f3734948
franchine <ninh.fran@gmail.com>
2022-09-16 20:53:38 -0700

    new app

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './css/App.css';
+
+function App() {
+  return (
+    <>
+    <div className='backgroundImg'>
+    <header className='App-header'>
+      franchine ninh <br></br>
+      <div className='App-subheader'><a className='App-link' href='www.google.com'> developer</a> | <a className='App-link' href='google.com'>artist</a></div>
+    </header>
+    </div>
+    
+    </>
+  );
+}
+
+export default App;
