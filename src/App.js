c6ab609e014a2bc5c1418b41d4a5fe586492df92
franchine <ninh.fran@gmail.com>
2023-04-21 17:49:27 -0700

    added more imgs

--- src/App.js
+++ src/App.js
@@ -11,12 +11,14 @@ const galleryImgs = [
   {
     img: require("./img/gallery/fatherson-edited.jpg"),
   },
-
+  {
+    img: require("./img/gallery/64630013-edited.JPG"),
+  },
   {
     img: require("./img/gallery/fluffy-edited.jpg"),
   },
   {
-    img: require("./img/gallery/hoian-edited.jpg"),
+    img: require("./img/gallery/64560013.JPG"),
   },
   {
     img: require("./img/gallery/mtrainer-edited.jpg"),
@@ -27,11 +29,41 @@ const galleryImgs = [
   {
     img: require("./img/gallery/tuilips026-edited.jpg"),
   },
-  {
-    img: require("./img/gallery/15650008-edited.png"),
-  },
+  // {
+  //   img: require("./img/gallery/15650008-edited.png"),
+  // },
   {
     img: require("./img/gallery/thevessel-edited.jpg"),
+  },
+  {
+    img: require("./img/gallery/Ninh411614-R1-063.jpg"),
+  },
+  {
+    img: require("./img/gallery/hoian-edited.jpg"),
+  },
+  {
+    img: require("./img/gallery/15650001.JPG"),
+  },
+  {
+    img: require("./img/gallery/64520027.JPG"),
+  },
+  {
+    img: require("./img/gallery/64570036.JPG"),
+  },
+  {
+    img: require("./img/gallery/Ninh002239-R1-027-12-edited.jpg"),
+  },
+  {
+    img: require("./img/gallery/64600034.JPG"),
+  },
+  {
+    img: require("./img/gallery/15650028.JPG"),
+  },
+  {
+    img: require("./img/gallery/Scan15808.jpg"),
+  },
+  {
+    img: require("./img/gallery/Ninh002240-R1-047-22-edited.jpg"),
   },
 
   // {
