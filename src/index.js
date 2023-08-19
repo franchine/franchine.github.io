b21e6832b9e428f4adff632b843c2a518515ac29
franchine <ninh.fran@gmail.com>
2023-08-19 16:25:41 -0700

    update base route

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter base="/">
+  <HashRouter base="/photgraphy-gallery/">
     <App />
   </HashRouter>
 );
