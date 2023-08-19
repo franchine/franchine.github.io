81717b8c1a294a2bb5c7d415ea173e37b259dd1e
franchine <ninh.fran@gmail.com>
2023-08-19 16:27:50 -0700

    fix typo lel

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter base="/photgraphy-gallery/">
+  <HashRouter base="/photography-gallery/">
     <App />
   </HashRouter>
 );
